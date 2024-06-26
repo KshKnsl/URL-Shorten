d2a8c72aae6da502f412eea577c3e85698a96631
Kush Knsl <mrkushkansal@gmail.com>
2024-06-26 22:59:24 +0530

    some fixes

--- server.js
+++ server.js
@@ -27,8 +27,7 @@ function generateShortLink()
         const randomIndex = Math.floor(Math.random() * randomString.length);
         shortLink += randomString[randomIndex];
     }
-    connection.query(`SELECT url FROM URLTABLE WHERE shortLink = '${shortLink}'`, function (err, results, fields) {
-        if (results.length <= 0)    return generateShortLink();
+    connection.query(`SELECT url FROM URLTABLE WHERE shortLink = $1`, [shortLink], function (err, results, fields) {if (results.rowCount <= 0)    return generateShortLink();
     });
     return shortLink;    
 }
@@ -36,18 +35,19 @@ app.post('/shorten', (req, res) =>
 {
     url  = req.body.url;
     if(!url || url.trim() =='')            res.redirect('/');
-    connection.query(`SELECT shortLink FROM URLTABLE WHERE url = '${url}'`, function (err, results, fields) 
+    connection.query(`SELECT shortLink FROM URLTABLE WHERE url = $1`, [url], function (err, results, fields) 
     {
-        if(results.length > 0) 
+        console.log(results);
+        if(results.rowCount > 0) 
         {
-            link = results[0].shortLink;
+            link = results.rows[0].shortLink;
             link = req.headers.origin + '/' + link;
             res.redirect('/');
         }
         else
         {
             link = generateShortLink();
-            connection.query(`INSERT INTO URLTABLE (url, shortLink) VALUES ('${url}', '${link}')`);
+            connection.query(`INSERT INTO URLTABLE (url, shortLink) VALUES ($1, $2)`, [url, link]);
             link = req.headers.origin + '/' + link;
             res.redirect('/');
         }
@@ -57,10 +57,12 @@ app.post('/shorten', (req, res) =>
 app.get('/:s', (req, res) => 
 {
     const shortLink = req.params.s;
-    connection.query(`SELECT url FROM URLTABLE WHERE shortLink = '${shortLink}'`, function (err, results, fields) {
-        if (results.length > 0) 
+    console.log(shortLink);
+    connection.query(`SELECT url FROM URLTABLE WHERE shortLink = $1`, [shortLink], function (err, results, fields) {
+        console.log(results);
+        if (results.rowCount > 0) 
         {
-            const url = results[0].url;
+            const url = results.rows[0].url;
             res.redirect(url);
         } 
         else  res.sendStatus(404);
